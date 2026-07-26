2377586afedf9f7e03577d393da5b92ed743f995
dhanesh0810 <pawardhanesh006@gmail.com>
2026-07-26 22:42:22 +0530

    My first commit of a sample playwright testcase

---
+++ tests/saucedemo.spec.ts
@@ -0,0 +1,17 @@
+import {test, expect, Locator} from '@playwright/test'
+
+test('Verify the login functionality on Sauce Demo page', async ({page}) => {
+
+    await page.goto('https://www.saucedemo.com/');
+
+    const username: Locator = page.locator('#user-name');
+    const password: Locator = page.locator('#password');
+    const loginButton: Locator = page.locator('#login-button');
+
+    await username.fill('standard_user');
+    await password.fill('secret_sauce');
+    await loginButton.click();
+
+    // npx playwright test tests/example.spec.ts --project=chromium --headed
+
+});
