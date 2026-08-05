01f5e2703104afef0e589b4f6b5e780d96cef04c
dhanesh0810 <pawardhanesh006@gmail.com>
2026-08-05 23:12:06 +0530

    Added changes to run headless mode in github workflows

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    headless: false, // --headed
+    headless: process.env.CI ? true : false, // --headed
     launchOptions:{
       slowMo: 500
     },
