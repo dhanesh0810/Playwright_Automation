0e6a4d009e0a1c78cb7941512c902724cb14dc21
dhanesh0810 <pawardhanesh006@gmail.com>
2026-08-15 00:10:58 +0530

    Dynamic tests using data driven testing

--- tests/LoginTest.spec.ts
+++ tests/LoginTest.spec.ts
@@ -2,24 +2,35 @@ import {expect, test} from '@playwright/test'
 import { LoginPage } from '../pages/LoginPage'
 import LoginData from '../test-data/LoginData.json'
 
-test('Verify the login functionality on Sauce Demo page with valid user', async ({page}) => {
+LoginData.forEach((data) => {
 
-    const loginpage  = new LoginPage(page);
+    if(!data.run) return;
 
-    await loginpage.gotoLoginPage();
-    await loginpage.login(LoginData.valid_user.username, LoginData.valid_user.password);
-    // await loginpage.verifyLoginSuccess();
-    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+    test(`Verify the login functionality on Sauce Demo page - ${data.username}`, async ({page}) => {
+
+        const loginpage = new LoginPage(page);
+
+        await loginpage.gotoLoginPage();
+        await loginpage.login(data.username, data.password);
+
+        if(data.expected === 'success'){
+            await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+        }else{
+            await expect(loginpage.errormessage).toBeVisible();
+        }
+
+    });
 
 });
 
-test('Verify the login functionality on Sauce Demo page with invalid user', async ({page}) => {
 
-    const loginpage  = new LoginPage(page);
+// test('Verify the login functionality on Sauce Demo page with invalid user', async ({page}) => {
 
-    await loginpage.gotoLoginPage();
-    await loginpage.login(LoginData.invalid_user.username, LoginData.invalid_user.password);
-    // await loginpage.verifyLoginSuccess();
-    await expect(loginpage.errormessage).toBeVisible();
+//     const loginpage  = new LoginPage(page);
 
-});
+//     await loginpage.gotoLoginPage();
+//     await loginpage.login(LoginData.invalid_user.username, LoginData.invalid_user.password);
+//     // await loginpage.verifyLoginSuccess();
+//     await expect(loginpage.errormessage).toBeVisible();
+
+// });
